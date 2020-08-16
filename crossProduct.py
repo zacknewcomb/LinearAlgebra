8a5f094bf8702b885f4f555954a917962826c13e
Zack <zack.newcomb1@gmail.com>
2020-08-16 13:53:19 -0400

    update crossProduct.py

--- crossProduct.py
+++ crossProduct.py
@@ -3,15 +3,6 @@ def cross_prod(v1,v2):
     j = -1* ((v2[2]*v1[0])-(v2[0]*v1[2]))
     k = (v2[1]*v1[0])-(v2[0]*v1[1])
     
-    return (i, j, k)
-
-def main():
-    v1 = list(map(int, input("First vector: ").strip().split()))
-    v2 = list(map(int, input("Second vector: ").strip().split()))
-    i, j, k = cross_prod(v1,v2)
-    print( str(i) + 'i ' + str(j) + 'j ' + str(k) + 'k')
-    
-    
-main()
+    return ([i, j, k])
     
     
