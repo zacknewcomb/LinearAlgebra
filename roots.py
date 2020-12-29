3f91cfdbd477617bbd01a1e7b62243f07adb8f57
Zack <zack.newcomb1@gmail.com>
2020-12-28 23:53:03 -0500

    Add files via upload

---
+++ roots.py
@@ -0,0 +1,13 @@
+def secant(f, x0, x1, n = 50, accuracy = 10**-6, currIteration = 0):
+    if n <= 0:
+        raise Exception("Number of Iterations performed in Secant method must be greater than 0, you input " + str(n))
+    
+    if abs(x1-x0) <= accuracy:
+        return x1
+    
+    x2 = x1 - f(x1) * (x1-x0) / (f(x1) - f(x0))
+    currIteration += 1
+    if currIteration < n:
+        root = secant(f,x1,x2,n,accuracy,currIteration)
+        
+    return root
