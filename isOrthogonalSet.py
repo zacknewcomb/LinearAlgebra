ff8b54b06f6922a956983e5bcdb5dad8dc2c122f
Zack <zack.newcomb1@gmail.com>
2020-12-30 21:34:15 -0500

    Delete isOrthogonalSet.py

--- isOrthogonalSet.py
+++
@@ -1,9 +0,0 @@
-from . import dotProduct
-
-def isOrthogonalSet(S):
-    for i in range(len(S)):
-        for j in range(i+1,len(S)):
-            if dotProduct.dot_prod(S[i], S[j]) != 0:
-                return False
-    
-    return True
