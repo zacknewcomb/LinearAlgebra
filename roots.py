4331ec65f5edb0c2f0a8e0f44831652812def4d9
Zack <zack.newcomb1@gmail.com>
2021-01-01 15:16:14 -0500

    add newton method

--- roots.py
+++ roots.py
@@ -11,3 +11,17 @@ def secant(f, x0, x1, n = 50, accuracy = 10**-6, currIteration = 0):
         root = secant(f,x1,x2,n,accuracy,currIteration)
         
     return root
+
+def newton(f, fprime, x0, n = 50, accuracy = 10**-6, currIteration = 0):
+    if n <= 0:
+        raise Exception("Number of Iterations performed in Newton's method must be greater than 0, you input " + str(n)) 
+    
+    x1 = x0 - (f(x0) / fprime(x0))
+    
+    if abs(x1-x0) <= accuracy:
+        return x1
+    
+    if currIteration < n:
+            root = newton(f,fprime,x1,n,accuracy,currIteration)
+            
+    return root
