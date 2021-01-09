5dc4e610c25ce4991dce6b6c4abb73887cc6e4f8
Zack <zack.newcomb1@gmail.com>
2021-01-08 21:17:08 -0500

    improve secant and newton methods

--- roots.py
+++ roots.py
@@ -1,27 +1,32 @@
-def secant(f, x0, x1, n = 50, accuracy = 10**-6, currIteration = 0):
+def secant(f, x0, x1, n = 50, accuracy = 10**-6):
     if n <= 0:
         raise Exception("Number of Iterations performed in Secant method must be greater than 0, you input " + str(n))
     
-    if abs(x1-x0) <= accuracy:
-        return x1
-    
-    x2 = x1 - f(x1) * (x1-x0) / (f(x1) - f(x0))
-    currIteration += 1
-    if currIteration < n:
-        root = secant(f,x1,x2,n,accuracy,currIteration)
+    currIteration = 0
+    while currIteration < n:
+        x2 = x1 - f(x1) * (x1-x0) / (f(x1) - f(x0))
+        x0 = x1
+        x1 = x2
+        currIteration += 1
         
-    return root
+        if abs(x1-x0) <= accuracy:
+            return x1
+        
+    return x1
 
-def newton(f, fprime, x0, n = 50, accuracy = 10**-6, currIteration = 0):
+def newton(f, fprime, x0, n = 50, accuracy = 10**-6):
     if n <= 0:
         raise Exception("Number of Iterations performed in Newton's method must be greater than 0, you input " + str(n)) 
     
-    x1 = x0 - (f(x0) / fprime(x0))
+
+    currIteration = 0
+    while currIteration < n:
+        x1 = x0 - (f(x0) / fprime(x0))
+        currIteration += 1
     
-    if abs(x1-x0) <= accuracy:
-        return x1
+        if abs(x1-x0) <= accuracy:
+            return x1
+        
+        x0 = x1
     
-    if currIteration < n:
-            root = newton(f,fprime,x1,n,accuracy,currIteration)
-            
-    return root
+    return x1
